55dba6b75b50fe39d6d7e86da51465827d639228
dayasudhan <dayasudhankg@gmail.com>
2023-09-15 15:53:39 +0530

    temp working

--- screens/InputScreen.js
+++ screens/InputScreen.js
@@ -48,12 +48,12 @@ const InputScreen = () => {
         validationSchema={validationSchema}
         onSubmit={(values) => onSubmitHandler(values)}
       >
-        {({ handleChange, handleSubmit, values, errors, touched }) => (
+        {({ handleChange, handleSubmit,handleBlur, values, errors, touched }) => (
           <KeyboardAwareScrollView
           style={styles.content}
           showsVerticalScrollIndicator={false}
           >
-            <View style={styles.formGroup}>
+            {/* <View style={styles.formGroup}> */}
             <Text style={styles.label}>Customer Details</Text>
             <TextInput
               style={styles.input}
@@ -62,11 +62,188 @@ const InputScreen = () => {
               value={values.name}
             />
             {touched.name && errors.name && <Text style={styles.error}>{errors.name}</Text>}
-          </View>
-         
+          {/* </View> */}
+          {/* <View style={styles.formGroup}> */}
+            <TextInput
+                 style={styles.input}
+                 value={values.phone}
+                 placeholder="Phone.."
+                 onChangeText={handleChange("phone")}
+                //  onBlur={handleBlur("phone")}
+               />
+             {/* </View> */}
+             <TextInput
+                  style={styles.input}
+                  value={values.landMark}
+                  placeholder="LandMark.."
+                  onChangeText={handleChange("landMark")}
+                  // onBlur={handleBlur("email")}
+                  autoCapitalize="none"
+                />
+                <TextInput
+                  style={styles.input}
+                  value={values.city}
+                  placeholder="Village/City.."
+                  onChangeText={handleChange("city")}
+                 
+                  autoCapitalize="none"
+                />
+                <TextInput
+                  style={styles.input}
+                  value={values.address}
+                  placeholder="Address.."
+                  onChangeText={handleChange("address")}
+                  onBlur={handleBlur("address")}
+                  autoCapitalize="none"
+                />
+
+<View style={styles.formGroup}>
+                <Text style={styles.label}>Person To be Contacted</Text>
+
+                <TextInput
+                  style={styles.input}
+                  value={values.personToContact}
+                  placeholder="Contact Name.."
+                  onChangeText={handleChange("personToContact")}
+                  onBlur={handleBlur("personToContact")}
+                />
+
+                {/* <ErrorMessage
+                  errorValue={touched.name && errors.name}
+                /> */}
+              </View>
+              <View style={styles.formGroup}>
+                <TextInput
+                  style={styles.input}
+                  value={values.personToContactPhone}
+                  placeholder="Contact Phone.."
+                  onChangeText={handleChange("name")}
+                  onBlur={handleBlur("name")}
+                />
+              </View>
+              <View style={styles.formGroup}>
+                <Text style={styles.label}>Property to be serviced</Text>
+
+                <TextInput
+                  style={styles.input}
+                  value={values.firstName}
+                  placeholder="Name.."
+                  onChangeText={handleChange("name")}
+                  onBlur={handleBlur("name")}
+                />
+ </View>
+
+<View style={styles.formGroup}>
+  <Text style={styles.label}>Service Frequeny:</Text>
+
+  <TextInput
+    style={styles.input}
+    value={values.firstName}
+    placeholder="Name.."
+    onChangeText={handleChange("name")}
+    onBlur={handleBlur("name")}
+  />
+   </View>
+   <View style={styles.formGroup}>
+                <Text style={styles.label}>Service Start Date</Text>
+
+                <TextInput
+                  style={styles.input}
+                  value={values.firstName}
+                  placeholder="Name.."
+                  onChangeText={handleChange("name")}
+                  onBlur={handleBlur("name")}
+                />
+
+                {/* <ErrorMessage
+                  errorValue={touched.name && errors.name}
+                /> */}
+              </View>
+              <View style={styles.formGroup}>
+                <Text style={styles.label}>Service End Date</Text>
+
+                <TextInput
+                  style={styles.input}
+                  value={values.firstName}
+                  placeholder="Name.."
+                  onChangeText={handleChange("name")}
+                  onBlur={handleBlur("name")}
+                />
+
+                {/* <ErrorMessage
+                  errorValue={touched.name && errors.name}
+                /> */}
+              </View>
+              <View style={styles.formGroup}>
+                <Text style={styles.label}>Reneval</Text>
+
+                <TextInput
+                  style={styles.input}
+                  value={values.firstName}
+                  placeholder="Name.."
+                  onChangeText={handleChange("name")}
+                  onBlur={handleBlur("name")}
+                />
+
+                {/* <ErrorMessage
+                  errorValue={touched.name && errors.name}
+                /> */}
+              </View>
+
+              <View style={styles.formGroup}>
+                <Text style={styles.label}>Pests to be Controlled</Text>
+
+                <TextInput
+                  style={styles.input}
+                  value={values.pestsToControl}
+                  placeholder="Pests to be controlled.."
+                  onChangeText={handleChange("name")}
+                  onBlur={handleBlur("name")}
+                />
+
+                {/* <ErrorMessage
+                  errorValue={touched.name && errors.name}
+                /> */}
+              </View>
+              <View style={styles.formGroup}>
+                <Text style={styles.label}>Payment Terms</Text>
+
+                <TextInput
+                  style={styles.input}
+                  value={values.paymentTerms}
+                  placeholder="Payment Terms.."
+                  onChangeText={handleChange("paymentTerms")}
+                  onBlur={handleBlur("paymentTerms")}
+                  multiline={true} // Set to true for multiline input
+                  numberOfLines={4} // Specify the number of lines to display (optional)
+                />
+
+                {/* <ErrorMessage
+                  errorValue={touched.name && errors.name}
+                /> */}
+              </View>
+              <View style={styles.formGroup}>
+                <Text style={styles.label}>Billing Instructions</Text>
+
+                <TextInput
+                  style={styles.input}
+                  value={values.billingInstructions}
+                  placeholder="Billing Instructions.."
+                  onChangeText={handleChange("billingInstructions")}
+                  onBlur={handleBlur("billingInstructions")}
+                   multiline={true} // Set to true for multiline input
+          numberOfLines={4} // Specify the number of lines to display (optional)
+                />
+
+                {/* <ErrorMessage
+                  errorValue={touched.name && errors.name}
+                /> */}
+              </View>
+
+
           <TouchableOpacity style={styles.button} onPress={handleSubmit}>
-                <Text style={styles.buttonText}>SUBMIT234</Text>
-              </TouchableOpacity>
+            <Text style={styles.buttonText}>SUBMIT</Text>
+          </TouchableOpacity>
           </KeyboardAwareScrollView>
         )}
       </Formik>
--- utils/validation.js
+++ utils/validation.js
@@ -3,14 +3,14 @@ import * as Yup from "yup";
 // https://github.com/jquense/yup
 export const validationSchema = Yup.object().shape({
   name: Yup.string().required("Input is required"),
-  email: Yup.string()
-    .email("Enter a valid email")
-    .required("Please enter a registered email"),
-  password: Yup.string()
-    .required()
-    .min(6, "Password must have at least 6 characters "),
-  confirmPassword: Yup.string().oneOf(
-    [Yup.ref("password"), null],
-    "Password & Confirm Password does not match"
-  ),
+  // email: Yup.string()
+  //   .email("Enter a valid email")
+  //   .required("Please enter a registered email"),
+  // password: Yup.string()
+  //   .required()
+  //   .min(6, "Password must have at least 6 characters "),
+  // confirmPassword: Yup.string().oneOf(
+  //   [Yup.ref("password"), null],
+  //   "Password & Confirm Password does not match"
+  // ),
 });
